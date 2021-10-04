42453cf6af75d1ef5a4ce61319fdc8a32e9ffa2d
vikas <khanchandani58@gmail.com>
2021-10-04 21:17:44 +0100

    images animation resolved

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -68,34 +68,115 @@ const Weather = (props) => {
       <p>{time}</p>
       <p>{tiempo.name}</p>
       {/* {renderizadorConditional()} */}
-      {(code === 800 && hours >= 6 && hours < 20) ? <img src={clearDay} alt="situation" /> : null}
-      {(code === 800 && hours >= 20 && hours < 6) ? <img src={clearNight} alt="situation" /> : null}
+      {code === 800 && (hours >= 6 && hours) < 20 ? (
+        <img src={clearDay} alt="situation" />
+      ) : null}
 
-      {(code === 741) ? <img src={fog} alt="situation" /> : null}
+      {code === 800 && hours >= 20 ? (
+        <img src={clearNight} alt="situation" />
+      ) : null}
+      {code === 800 && hours < 6 ? (
+        <img src={clearNight} alt="situation" />
+      ) : null}
 
-      {(code >= 612 && code <= 622) ? <img src={hail} alt="situation" /> : null}
+      {code === 741 ? <img src={fog} alt="situation" /> : null}
 
-      {(code >= 300 && code <= 321) ? <img src={showers} alt="situation" /> : null}
+      {code >= 612 && code <= 622 ? <img src={hail} alt="situation" /> : null}
 
-      {(code >= 600 && code <= 622) ? <img src={sleet} alt="situation" /> : null}
+      {code >= 300 && code <= 321 ? (
+        <img src={showers} alt="situation" />
+      ) : null}
 
-      {(code === 611) ? <img src={snow} alt="situation" /> : null}
+      {code >= 600 && code <= 622 ? <img src={sleet} alt="situation" /> : null}
 
-      {((code === 803 || code === 804) && (hours >= 6 && hours < 20)) ? <img src={heavyCloudDay} alt="situation" /> : null}
-      {((code === 803 || code === 804) && (hours >= 20 && hours < 6)) ? <img src={heavyCloudNight} alt="situation" /> : null}
+      {code === 611 ? <img src={snow} alt="situation" /> : null}
 
-      {((code === 801 || code === 802) && (hours >= 20 && hours < 6)) ? <img src={lightCloudNight} alt="situation" /> : null}
-      {((code === 801 || code === 802) && (hours >= 6 && hours < 20)) ? <img src={lightCloudDay} alt="situation" /> : null}
+      {code === 803 && hours >= 6 && hours < 20 ? (
+        <img src={heavyCloudDay} alt="situation" />
+      ) : null}
 
+      {code === 804 && hours >= 6 && hours < 20 ? (
+        <img src={heavyCloudDay} alt="situation" />
+      ) : null}
 
-      {((code >= 502 && code <= 531) && (hours >= 6 && hours < 20)) ? <img src={heavyRainDay} alt="situation" /> : null}
-      {((code >= 502 && code <= 531) && (hours >= 20 && hours < 6)) ? <img src={heavyRainNight} alt="situation" /> : null}
+      {code === 803 && hours >= 20 ? (
+        <img src={heavyCloudNight} alt="situation" />
+      ) : null}
+      {code === 803 && hours < 6 ? (
+        <img src={heavyCloudNight} alt="situation" />
+      ) : null}
+      {code === 804 && hours >= 20 ? (
+        <img src={heavyCloudNight} alt="situation" />
+      ) : null}
+      {code === 804 && hours < 6 ? (
+        <img src={heavyCloudNight} alt="situation" />
+      ) : null}
 
-      {((code === 500 || code === 501) && (hours >= 6 && hours < 20)) ? <img src={lightRainDay} alt="situation" /> : null}
-      {((code === 500 || code === 501) && (hours >= 20 && hours < 6)) ? <img src={lightRainNight} alt="situation" /> : null}
+      {code === 801 && hours >= 20 ? (
+        <img src={lightCloudNight} alt="situation" />
+      ) : null}
+      {code === 801 && hours < 6 ? (
+        <img src={lightCloudNight} alt="situation" />
+      ) : null}
 
-      {((code >= 200 && code <= 232) && (hours >= 6 && hours < 20)) ? <img src={thunderstormDay} alt="situation" /> : null}
-      {((code >= 200 && code <= 232) && (hours >= 20 && hours < 6)) ? <img src={thunderstormNight} alt="situation" /> : null}
+      {code === 802 && hours >= 20 ? (
+        <img src={lightCloudNight} alt="situation" />
+      ) : null}
+      {code === 802 && hours < 6 ? (
+        <img src={lightCloudNight} alt="situation" />
+      ) : null}
+
+      {code === 801 && hours >= 6 && hours < 20 ? (
+        <img src={lightCloudDay} alt="situation" />
+      ) : null}
+
+      {code === 802 && hours >= 6 && hours < 20 ? (
+        <img src={lightCloudDay} alt="situation" />
+      ) : null}
+
+      {code >= 502 && code <= 531 && hours >= 6 && hours < 20 ? (
+        <img src={heavyRainDay} alt="situation" />
+      ) : null}
+
+      {code >= 502 && code <= 531 && hours >= 20 ? (
+        <img src={heavyRainNight} alt="situation" />
+      ) : null}
+      {code >= 502 && code <= 531 && hours < 6 ? (
+        <img src={heavyRainNight} alt="situation" />
+      ) : null}
+
+      {code === 500 && hours >= 6 && hours < 20 ? (
+        <img src={lightRainDay} alt="situation" />
+      ) : null}
+
+      {code === 501 && hours >= 6 && hours < 20 ? (
+        <img src={lightRainDay} alt="situation" />
+      ) : null}
+
+      {code === 500 && hours >= 20 ? (
+        <img src={lightRainNight} alt="situation" />
+      ) : null}
+      {code === 500 && hours < 6 ? (
+        <img src={lightRainNight} alt="situation" />
+      ) : null}
+
+      {code === 501 && hours >= 20 ? (
+        <img src={lightRainNight} alt="situation" />
+      ) : null}
+      {code === 501 && hours < 6 ? (
+        <img src={lightRainNight} alt="situation" />
+      ) : null}
+
+      {code >= 200 && code <= 232 && hours >= 6 && hours < 20 ? (
+        <img src={thunderstormDay} alt="situation" />
+      ) : null}
+
+      {code >= 200 && code <= 232 && hours >= 20 ? (
+        <img src={thunderstormNight} alt="situation" />
+      ) : null}
+      {code >= 200 && code <= 232 && hours < 6 ? (
+        <img src={thunderstormNight} alt="situation" />
+      ) : null}
 
       <p>{tiempo.weather[0].description}</p>
       <p style={{ fontWeight: "500", fontSize: "3rem" }}>
@@ -133,7 +214,7 @@ const Weather = (props) => {
           {/* percipitation */}
           <i className="fas fa-cloud-rain fa-2x"></i>
           <br />
-          {tiempo.main.pressure} 
+          {tiempo.main.pressure}
         </p>
       </div>
     </div>
